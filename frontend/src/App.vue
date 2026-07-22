117b3b7ece2ca3a0b49766a0ff345d41580cae69
Guilherme Branco <guiguiguiduarte@gmail.com>
2026-07-22 16:57:51 +0100

    animation fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,5 +2,21 @@
 </script>
 
 <template>
-  <RouterView />
-</template>
+<RouterView v-slot="{ Component }">
+    <Transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
+  </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
